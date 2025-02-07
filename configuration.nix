bb1e7bb61c27f5d88741708e11f3ae79f5048441
OleksandrDreval <drevaloleksandr@gmail.com>
2025-02-07 23:22:17 +0200

    Enhance system security and sysctl configuration
    - Expand and refine kernel sysctl settings with comprehensive security parameters
    - Add new network, filesystem, and kernel protection configurations
    - Modify trusted and allowed users for Nix
    - Enable EFI and Secure Boot for virtualization
    - Update comments and explanations for new security settings

--- configuration.nix
+++ configuration.nix
@@ -70,35 +70,89 @@
 
     # Безпекові налаштування парметрів ядра sysctl
     kernel.sysctl = {
-      "dev.tty.ldisc_autoload"                      = 0;      # Вимкнення автоматичного завантаження лінійних дисциплін для терміналів. Це покращує безпеку, запобігаючи завантаженню шкідливих лінійних дисциплін.
-      "kernel.dmesg_restrict"                       = 1;      # Обмеження доступу до dmesg
-      "kernel.ftrace_enabled"                       = false;  # Вимкнення ftrace для покращення безпеки та продуктивності. ftrace - це інструмент трасування ядра, який може бути використаний для атак.
-      "kernel.kptr_restrict"                        = 2;      # Обмеження доступу до адрес ядра. Рівень 2 забезпечує максимальний захист.
-      "kernel.perf_event_paranoid"                  = 3;      # Параноїдальний режим для perf events. Це запобігає використанню perf events для атак.
-      "kernel.randomize_va_space"                   = 2;      # Рандомізація адресного простору. Рівень 2 забезпечує більш сильну рандомізацію.
-      "kernel.sysrq"                                = 4;      # Обмеження можливостей магічної клавіші SysRq. Рівень 4 дозволяє лише деякі функції.
-      "kernel.unprivileged_bpf_disabled"            = 1;      # Вимкнення BPF для непривілейованих користувачів. Це запобігає використанню BPF для атак.
-      "kernel.yama.ptrace_scope"                    = 2;      # Обмеження ptrace. Рівень 2 дозволяє ptrace тільки для процесів з тим самим UID.
-      "net.core.bpf_jit_enable"                     = false;  # Вимкнення JIT для BPF. Це покращує безпеку, запобігаючи використанню JIT для атак.
-      "net.core.bpf_jit_harden"                     = 2;      # Зміцнення JIT для BPF. Рівень 2 забезпечує більш сильне зміцнення.
-      "net.ipv4.conf.all.accept_redirects"          = false;  # Не приймати ICMP redirects. Це запобігає атакам маршрутизації.
-      "net.ipv4.conf.all.log_martians"              = true;   # Логувати "марсіанські" пакети. Це допомагає виявити атаки.
-      "net.ipv4.conf.all.rp_filter"                 = 1;      # Reverse path filtering. Це допомагає запобігти атакам підробки IP-адрес.
-      "net.ipv4.conf.all.secure_redirects"          = false;  # Не приймати redirects від будь-кого. Це запобігає атакам маршрутизації.
-      "net.ipv4.conf.all.send_redirects"            = false;  # Не надсилати redirects. Це запобігає атакам маршрутизації.
-      "net.ipv4.conf.default.accept_redirects"      = false;  # accept_redirects, але для інтерфейсу за замовчуванням
-      "net.ipv4.conf.default.log_martians"          = true;   # log_martians, але для інтерфейсу за замовчуванням
-      "net.ipv4.conf.default.rp_filter"             = 1;      # rp_filter, але для інтерфейсу за замовчуванням
-      "net.ipv4.conf.default.secure_redirects"      = false;  # secure_redirects, але для інтерфейсу за замовчуванням
-      "net.ipv4.conf.default.send_redirects"        = false;  # send_redirects, але для інтерфейсу за замовчуванням
-      "net.ipv4.icmp_echo_ignore_all"               = 1;      # Ігнорувати всі ICMP echo запити (ping). Це покращує безпеку, запобігаючи DoS атакам.
-      "net.ipv4.icmp_echo_ignore_broadcasts"        = 1;      # Ігнорувати broadcast ping
-      "net.ipv4.icmp_ignore_bogus_error_responses"  = 1;      # Ігнорувати неправильні ICMP повідомлення про помилки. Це допомагає запобігти атакам.
-      "net.ipv4.tcp_rfc1337"                        = 1;      # Вмикає захист від TCP атак, описаних в RFC 1337.
-      "net.ipv4.tcp_syncookies"                     = 1;      # Увімкнення SYN cookies для захисту від SYN flood
-      "net.ipv6.conf.all.accept_redirects"          = false;  # Не приймати IPv6 redirects. Це запобігає атакам маршрутизації.
-      "net.ipv6.conf.default.accept_redirects"      = false;  # те саме для IPv6 інтерфейсу за замовчуванням
-      "vm.unprivileged_userfaultfd"                 = 0;      # Вимкнення userfaultfd для непривілейованих користувачів. Це покращує безпеку, запобігаючи використанню userfaultfd для атак.
+      "kernel.unprivileged_userns_clone"            = "0";
+
+      "fs.binfmt_misc.status"                       = "0";
+      "kernel.io_uring_disabled"                    = "2";
+      "net.ipv4.ip_forward"                         = "0";
+      "net.ipv4.conf.all.forwarding"                = "0";
+      "net.ipv4.conf.default.forwarding"            = "0";
+      "net.ipv6.conf.all.forwarding"                = "0";
+      "net.ipv6.conf.default.forwarding"            = "0";
+      "net.ipv4.tcp_timestamps"                     = "1";
+      "net.ipv4.conf.all.accept_source_route"       = "0";
+      "net.ipv4.conf.default.accept_source_route"   = "0";
+      "net.ipv6.icmp_echo_ignore_all"               = "1";
+      "net.ipv4.tcp_dsack"                          = "0";
+      "net.ipv4.tcp_fack"                           = "0";
+      "net.ipv4.tcp_sack"                           = "0";
+      "net.ipv6.conf.all.accept_ra"                 = "0";
+      "net.ipv6.conf.all.accept_source_route"       = "0";
+      "net.ipv6.conf.default.accept_source_route"   = "0";
+      "net.ipv6.default.accept_ra"                  = "0";
+      "kernel.core_pattern"                         = "|/bin/false";
+      "vm.mmap_rnd_bits"                            = "32";
+      "vm.mmap_rnd_compat_bits"                     = "16";
+      "fs.protected_fifos"                          = "2";
+      "fs.protected_hardlinks"                      = "1";
+      "fs.protected_regular"                        = "2";
+      "fs.protected_symlinks"                       = "1";
+      "fs.suid_dumpable"                            = "0";
+      "kernel.kexec_load_disabled"                  = "1";
+      "kernel.printk"                               = "3 3 3 3";
+      "kernel.perf_cpu_time_max_percent"            = "1";
+      "kernel.perf_event_max_sample_rate"           = "1";
+      "vm.mmap_min_addr"                            = "65536";
+      "net.ipv4.conf.default.shared_media"          = "0";
+      "net.ipv4.conf.all.shared_media"              = "0";
+      "net.ipv4.conf.default.arp_announce"          = "2";
+      "net.ipv4.conf.all.arp_announce"              = "2";
+      "net.ipv4.conf.default.arp_ignore"            = "1";
+      "net.ipv4.conf.all.arp_ignore"                = "1";
+      "net.ipv4.conf.default.drop_gratuitous_arp"   = "1";
+      "net.ipv4.conf.all.drop_gratuitous_arp"       = "1";
+      "net.ipv6.conf.default.router_solicitations"  = "0";
+      "net.ipv6.conf.all.router_solicitations"      = "0";
+      "net.ipv6.conf.default.accept_ra_rtr_pref"    = "0";
+      "net.ipv6.conf.all.accept_ra_rtr_pref"        = "0";
+      "net.ipv6.conf.default.accept_ra_pinfo"       = "0";
+      "net.ipv6.conf.all.accept_ra_pinfo"           = "0";
+      "net.ipv6.conf.default.accept_ra_defrtr"      = "0";
+      "net.ipv6.conf.all.accept_ra_defrtr"          = "0";
+      "net.ipv6.conf.default.autoconf"              = "0";
+      "net.ipv6.conf.all.autoconf"                  = "0";
+      "net.ipv6.conf.default.dad_transmits"         = "0";
+      "net.ipv6.conf.all.dad_transmits"             = "0";
+
+      "dev.tty.ldisc_autoload"                      = "0";      # Вимкнення автоматичного завантаження лінійних дисциплін для терміналів. Це покращує безпеку, запобігаючи завантаженню шкідливих лінійних дисциплін.
+      "kernel.dmesg_restrict"                       = "1";      # Обмеження доступу до dmesg
+      "kernel.ftrace_enabled"                       = "false";  # Вимкнення ftrace для покращення безпеки та продуктивності. ftrace - це інструмент трасування ядра, який може бути використаний для атак.
+      "kernel.kptr_restrict"                        = "2";      # Обмеження доступу до адрес ядра. Рівень 2 забезпечує максимальний захист.
+      "kernel.perf_event_paranoid"                  = "3";      # Параноїдальний режим для perf events. Це запобігає використанню perf events для атак.
+      "kernel.randomize_va_space"                   = "2";      # Рандомізація адресного простору. Рівень 2 забезпечує більш сильну рандомізацію.
+      "kernel.sysrq"                                = "4";      # Обмеження можливостей магічної клавіші SysRq. Рівень 4 дозвляє лише деякі функції.
+      "kernel.unprivileged_bpf_disabled"            = "1";      # Вимкнення BPF для непривілейованих користувачів. Це запобігає використанню BPF для атак.
+      "kernel.yama.ptrace_scope"                    = "2";      # Обмеження ptrace. Рівень 2 дозволяє ptrace тільки для процесів з тим самим UID.
+      "net.core.bpf_jit_enable"                     = "false";  # Вимкнення JIT для BPF. Це покращує безпеку, запобігаючи використанню JIT для атак.
+      "net.core.bpf_jit_harden"                     = "2";      # Зміцнення JIT для BPF. Рівень 2 забезпечує більш сильне зміцнення.
+      "net.ipv4.conf.all.accept_redirects"          = "false";  # Не приймати ICMP redirects. Це запобігає атакам маршрутизації.
+      "net.ipv4.conf.all.log_martians"              = "true";   # Логувати "марсіанські" пакети. Це допомагає виявити атаки.
+      "net.ipv4.conf.all.rp_filter"                 = "1";      # Reverse path filtering. Це допомагає запобігти атакам підробки IP-адрес.
+      "net.ipv4.conf.all.secure_redirects"          = "false";  # Не приймати redirects від будь-кого. Це запобігає атакам маршрутизації.
+      "net.ipv4.conf.all.send_redirects"            = "false";  # Не надсилати redirects. Це запобігає атакам маршрутизації.
+      "net.ipv4.conf.default.accept_redirects"      = "false";  # accept_redirects, але для інтерфейсу за замовчуванням
+      "net.ipv4.conf.default.log_martians"          = "true";   # log_martians, але для інтерфейсу за замовчуванням
+      "net.ipv4.conf.default.rp_filter"             = "1";      # rp_filter, але для інтерфейсу за замовчуванням
+      "net.ipv4.conf.default.secure_redirects"      = "false";  # secure_redirects, але для інтерфейсу за замовчуванням
+      "net.ipv4.conf.default.send_redirects"        = "false";  # send_redirects, але для інтерфейсу за замовчуванням
+      "net.ipv4.icmp_echo_ignore_all"               = "1";      # Ігнорувати всі ICMP echo запити (ping). Це покращує безпеку, запобігаючи DoS атакам.
+      "net.ipv4.icmp_echo_ignore_broadcasts"        = "1";      # Ігнорувати broadcast ping
+      "net.ipv4.icmp_ignore_bogus_error_responses"  = "1";      # Ігнорувати неправильні ICMP повідомлення про помилки. Це допомагає запобігти атакам.
+      "net.ipv4.tcp_rfc1337"                        = "1";      # Вмикає захист від TCP атак, описаних в RFC 1337.
+      "net.ipv4.tcp_syncookies"                     = "1";      # Увімкнення SYN cookies для захисту від SYN flood
+      "net.ipv6.conf.all.accept_redirects"          = "false";  # Не приймати IPv6 redirects. Це запобігає атакам маршрутизації.
+      "net.ipv6.conf.default.accept_redirects"      = "false";  # те саме для IPv6 інтерфейсу за замовчуванням
+      "vm.unprivileged_userfaultfd"                 = "0";      # Вимкнення userfaultfd для непривілейованих користувачів. Це покращує безпеку, запобігаючи використанню userfaultfd для атак.
     };
 
     /* Бан-лист небезпечних або застрілих модулів ядра */
@@ -207,7 +261,7 @@
         ip6tables -A INPUT -p icmpv6 --icmpv6-type echo-request -j DROP
       '';
 
-                       # Очищення правил IPtables при зупинці
+                      # Очищення правил IPtables при зупинці
       /*  Використовуємо -F для видалення всіх правил у вказаних ланцюжках
           Використовуємо -X для видалення всіх користувацьких ланцюжків
           Це забезпечує чистий стан мережевого фільтру при перезапуску системи
@@ -515,12 +569,12 @@
     /*  Користувачі, яким дозволено виконувати привілейовані операції:
         - root: системний адміністратор
         - @wheel: користувачі з правами адміністратора  */
-    trusted-users                 = [ "root" "@wheel" ];
+    trusted-users                 = [ "@wheel" ];
 
     /*  Користувачі, яким дозволено використовувати Nix:
         - root: системний адміністратор
         - @wheel: користувачі з правами адміністратора  */
-    allowed-users                 = [ "root" "@wheel" ];
+    allowed-users                 = [ "@wheel" ];
   };
 
   /*  Дозвіл на використання пропрієтарного програмного забезпечення
@@ -566,7 +620,8 @@
 
   # ВІРТУАЛІЗАЦІЯ
   virtualisation = {
-
+    useEFIBoot = true;
+    useSecureBoot = true;
     # Налаштування libvirt для керування віртуалізацією
     libvirtd = {
       enable        = true;           # Увімкнення служби libvirt
